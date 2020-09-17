254d9fe0a0bf2dfaa5d3871322c9ecabfeda9f79
ByeongUk-An <quddnr005@naver.com>
2020-09-17 18:21:29 +0900

    반응형작업 슬라이드완료

--- Line/js/index.js
+++ Line/js/index.js
@@ -13,3 +13,21 @@ xbtn.addEventListener("click", () => {
   hambug.classList.remove("off");
   xbtn.classList.remove("on");
 });
+
+const imgarr = [
+  "./img/mline1.jpeg",
+  "./img/mline2.jpeg",
+  "./img/mline3.jpeg",
+  "./img/mline4.jpeg",
+];
+
+const mline1 = document.querySelector(".mline1");
+const mline2 = document.querySelector(".mline2");
+const mline3 = document.querySelector(".mline3");
+const mline4 = document.querySelector(".mline4");
+if (window.innerWidth <= 480) {
+  mline1.src = imgarr[0];
+  mline2.src = imgarr[1];
+  mline3.src = imgarr[2];
+  mline4.src = imgarr[3];
+}
