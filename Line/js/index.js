7319b8c549eb81166aa9cc2e7082544197428cda
ByeongUk-An <quddnr005@naver.com>
2020-09-18 10:29:11 +0900

    resize 될 때 src값 변경

--- Line/js/index.js
+++ Line/js/index.js
@@ -20,14 +20,39 @@ const imgarr = [
   "./img/mline3.jpeg",
   "./img/mline4.jpeg",
 ];
+const imgarr1 = [
+  "./img/line1.jpeg",
+  "./img/line2.jpeg",
+  "./img/line3.jpeg",
+  "./img/line4.jpeg",
+];
 
 const mline1 = document.querySelector(".mline1");
 const mline2 = document.querySelector(".mline2");
 const mline3 = document.querySelector(".mline3");
 const mline4 = document.querySelector(".mline4");
-if (window.innerWidth <= 480) {
+if (window.innerWidth <= 900) {
   mline1.src = imgarr[0];
   mline2.src = imgarr[1];
   mline3.src = imgarr[2];
   mline4.src = imgarr[3];
+} else {
+  mline1.src = imgarr1[0];
+  mline2.src = imgarr1[1];
+  mline3.src = imgarr1[2];
+  mline4.src = imgarr1[3];
 }
+
+window.onresize = () => {
+  if (window.innerWidth <= 900) {
+    mline1.src = imgarr[0];
+    mline2.src = imgarr[1];
+    mline3.src = imgarr[2];
+    mline4.src = imgarr[3];
+  } else {
+    mline1.src = imgarr1[0];
+    mline2.src = imgarr1[1];
+    mline3.src = imgarr1[2];
+    mline4.src = imgarr1[3];
+  }
+};
